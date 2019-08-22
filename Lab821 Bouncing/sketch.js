1c2620f78a44d99395bbb44630d98c06e3b8adf3
Nayan Smuek <nsmuek@WS-ACADBUS.EDU>
2019-08-22 08:37:28 -0700

    Added an array and a loop to make adding more balls easier

--- Lab821 Bouncing/sketch.js
+++ Lab821 Bouncing/sketch.js
@@ -2,21 +2,22 @@
 // 	8/21/19
 //  This is a comment
 //  The setup function function is called once when your program begins
+var balls = [];
 function setup() {
   var cnv = createCanvas(800, 800);
   cnv.position((windowWidth-width)/2, 30);
   background(5, 5, 5);
   fill(200, 30, 150);
-  b1 = new Ball(random(width),random(height),random(-5,5),random(-5,5))
-  b2 = new Ball(random(width),random(height),random(-5,5),random(-5,5))
-  b3 = new Ball(random(width),random(height),random(-5,5),random(-5,5))
+  for(q=1;q<10;q++){
+  balls[q] = new Ball(random(width),random(height),random(-5,5),random(-5,5))
+}
 }
 
 //  The draw function is called @ 30 fps
 function draw() {
   //for(i=1;i<3;i++){} put other function inside this to run at 60fps
 background(5,5,5)//get rid of ball trails
-b1.run();
-b2.run();
-b3.run();
+  for(q=1;q<10;q++){
+balls[q].run();
+}
 }
