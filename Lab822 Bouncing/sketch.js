c868df524d440defd69464a383a37168cab5af77
NikolaTesla17 <44534701+NikolaTesla17@users.noreply.github.com>
2019-08-23 08:43:57 -0700

    Added orginizational folder and updated methods

---
+++ Lab822 Bouncing/sketch.js
@@ -0,0 +1,25 @@
+//  Nayan Smuek
+// 	8/21/19
+//  This is a comment
+//  The setup function function is called once when your program begins
+var balls = [];
+function setup() {
+  var cnv = createCanvas(800, 800);
+  cnv.position((windowWidth-width)/2, 30);
+  background(5, 5, 5);
+  fill(200, 30, 150);
+  for(q=1;q<10;q++){
+  balls[q] = new Ball(random(width),random(height),random(-5,5),random(-5,5))
+}
+img = loadImage('cat.png');
+}
+
+//  The draw function is called @ 30 fps
+function draw() {
+  //for(i=1;i<3;i++){} put other function inside this to run at 60fps
+background(5,5,5)//get rid of ball trails
+  for(q=1;q<10;q++){
+      image(img, 50, 50);
+balls[q].run();
+}
+}
