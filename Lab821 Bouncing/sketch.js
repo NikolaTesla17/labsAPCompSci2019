4b9eac0b5238f127513fcc3e9a76462ad60ade6d
NikolaTesla17 <44534701+NikolaTesla17@users.noreply.github.com>
2019-08-23 08:47:18 -0700

    Renamed files for consistancy

--- Lab821 Bouncing/sketch.js
+++ Lab821 Bouncing/sketch.js
@@ -1,28 +1,15 @@
 //  Nayan Smuek
 // 	8/21/19
 //  This is a comment
-//  The setup function function is called once when your program begins\
-ballsInit(i){
-for(q=0;q<i;q++){
-balls[q] = new Ball(random(width),random(height),random(-5,5),random(-5,5))
-}
-
-ballsRun(){
-  background(5,5,5)//get rid of ball trails
-    for(q=0;q<n;q++){
-        image(img, 50, 50);
-  balls[q].run();
-  }
-}
-
+//  The setup function function is called once when your program begins
 var balls = [];
-var n = 100; //change number of balls
 function setup() {
   var cnv = createCanvas(800, 800);
   cnv.position((windowWidth-width)/2, 30);
   background(5, 5, 5);
   fill(200, 30, 150);
-  ballsInit(n);
+  for(q=1;q<10;q++){
+  balls[q] = new Ball(random(width),random(height),random(-5,5),random(-5,5))
 }
 img = loadImage('cat.png');
 }
@@ -30,5 +17,9 @@ img = loadImage('cat.png');
 //  The draw function is called @ 30 fps
 function draw() {
   //for(i=1;i<3;i++){} put other function inside this to run at 60fps
-ballsRun(n)
+background(5,5,5)//get rid of ball trails
+  for(q=1;q<10;q++){
+      image(img, 50, 50);
+balls[q].run();
+}
 }
--- Lab822 Bouncing/sketch.js
+++ Lab822 Bouncing/sketch.js
@@ -1,25 +1,36 @@
 //  Nayan Smuek
 // 	8/21/19
 //  This is a comment
-//  The setup function function is called once when your program begins
+//  The setup function function is called once when your program begins\
+
 var balls = [];
+var n = 100; //change number of balls
 function setup() {
   var cnv = createCanvas(800, 800);
   cnv.position((windowWidth-width)/2, 30);
   background(5, 5, 5);
   fill(200, 30, 150);
-  for(q=1;q<10;q++){
-  balls[q] = new Ball(random(width),random(height),random(-5,5),random(-5,5))
+  ballsInit(n);
+  img = loadImage('cat.png');
 }
-img = loadImage('cat.png');
-}
+
 
 //  The draw function is called @ 30 fps
 function draw() {
   //for(i=1;i<3;i++){} put other function inside this to run at 60fps
-background(5,5,5)//get rid of ball trails
-  for(q=1;q<10;q++){
-      image(img, 50, 50);
-balls[q].run();
+ballsRun(n)
 }
+
+ballsInit(i){
+for(q=0;q<i;q++){
+balls[q] = new Ball(random(width),random(height),random(-5,5),random(-5,5))
+}
+}
+
+ballsRun(){
+  background(5,5,5)//get rid of ball trails
+    for(q=0;q<n;q++){
+        image(img, 50, 50);
+  balls[q].run();
+  }
 }
