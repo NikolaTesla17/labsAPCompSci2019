0bce520f5cfb026450af8050c8bf7224edc87cd4
NikolaTesla17 <44534701+NikolaTesla17@users.noreply.github.com>
2019-08-25 19:53:01 -0700

    code now, you know, works

--- Lab822 Bouncing/sketch.js
+++ Lab822 Bouncing/sketch.js
@@ -4,32 +4,31 @@
 //  The setup function function is called once when your program begins\
 
 var balls = [];
-var n = 100; //change number of balls
+var n = 10; //change number of balls
 function setup() {
   var cnv = createCanvas(800, 800);
   cnv.position((windowWidth-width)/2, 30);
   background(5, 5, 5);
   fill(200, 30, 150);
-  ballsInit(n);
+  // ballsInit(n);
   // img = loadImage('cat.png');
+  for(q=0;q<n;q++){
+    balls[q] = new Ball(random(width),random(height),random(-5,5),random(-5,5))
+  }
 }
 
 
 //  The draw function is called @ 30 fps
 function draw() {
   //for(i=1;i<3;i++){} put other function inside this to run at 60fps
-ballsRun(n)
+
+ballsDraw();
 }
 
-ballsInit(i)
-for(q=0;q<i;q++){
-balls[q] = new Ball(random(width),random(height),random(-5,5),random(-5,5))
-}
-
-
-ballsRun() 
+function ballsDraw(){
   background(5,5,5)//get rid of ball trails
-    for(q=0;q<n;q++){
-        image(img, 50, 50);
-  balls[q].run();
-  }
+  for(i=0;i<n;i++){
+balls[i].run();
+}
+}
+// ballsInit(i)
