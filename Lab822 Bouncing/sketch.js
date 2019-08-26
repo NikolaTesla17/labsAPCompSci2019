04fda2595395c1c1813cd8e211dd167e30fbeb4e
NikolaTesla17 <44534701+NikolaTesla17@users.noreply.github.com>
2019-08-25 19:57:10 -0700

    code is now cleaner with user input

--- Lab822 Bouncing/sketch.js
+++ Lab822 Bouncing/sketch.js
@@ -4,31 +4,26 @@
 //  The setup function function is called once when your program begins\
 
 var balls = [];
-var n = 10; //change number of balls
+var x = prompt("Number of Balls","42"); //user entered number of balls page dialog
 function setup() {
   var cnv = createCanvas(800, 800);
   cnv.position((windowWidth-width)/2, 30);
   background(5, 5, 5);
   fill(200, 30, 150);
-  // ballsInit(n);
-  // img = loadImage('cat.png');
-  for(q=0;q<n;q++){
+  for(q=0;q<x;q++){ //create array with number of balls wanted
     balls[q] = new Ball(random(width),random(height),random(-5,5),random(-5,5))
   }
 }
 
 
-//  The draw function is called @ 30 fps
 function draw() {
   //for(i=1;i<3;i++){} put other function inside this to run at 60fps
-
 ballsDraw();
 }
 
 function ballsDraw(){
   background(5,5,5)//get rid of ball trails
-  for(i=0;i<n;i++){
+  for(i=0;i<x;i++){
 balls[i].run();
 }
 }
-// ballsInit(i)
