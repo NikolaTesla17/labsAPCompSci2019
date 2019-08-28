81730fd5fae79f2b7b734b65598f5845391979fd
NikolaTesla17 <44534701+NikolaTesla17@users.noreply.github.com>
2019-08-28 08:49:01 -0700

    broke it

--- Lab828 Vectors/ball.js
+++ Lab828 Vectors/ball.js
@@ -1,9 +1,11 @@
 class Ball{
   constructor(x,y,dx,dy){
-    this.x = x;
-    this.y = y;
-    this.dx = dx;
-    this.dy = dy;
+    let v1 = createVector(x,y);
+    // this.x = x;
+    // this.y = y;
+    let v2 = createVector(dx,dy);
+    // this.dx = dx;
+    // this.dy = dy;
     this.clr = color(random(255),random(255),random(255))
   }
   run(){//use this so you dont have to run all of these for each ball
@@ -12,25 +14,25 @@ class Ball{
     this.render();
   }
   checkEdges(){//check if touching edge, if so reverse direction
-    if(this.x<0){
-      this.dx = this.dx*-1;//left
+    if(v1.x<0){
+      v2.dx = v2.dx*-1;//left
     }
-    if(this.x>width){
-      this.dx = this.dx*-1;//right
+    if(v1.x>width){
+      v2.dx = v2.dx*-1;//right
     }
-    if(this.y<0){
-      this.dy = this.dy*-1;//bottom
+    if(v1.y<0){
+      v2.dy = v2.dy*-1;//bottom
     }
-    if(this.y>height){
-      this.dy = this.dy*-1;//top
+    if(v1.y>height){
+      v2.dy = v2.dy*-1;//top
     }
   }
   update(){//update location
-    this.x = this.x + this.dx;
-    this.y = this.y + this.dy;
+    v1.x = v1.x + v2.dx;
+    v1.y = v1.y + v2.dy;
   }
   render(){//render one frame
     fill(this.clr);
-    ellipse(this.x,this.y,50,50)
+    ellipse(v1.x,v1.y,50,50)
   }
 }
