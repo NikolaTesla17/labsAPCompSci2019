a30796d7db8dfd1a012d094e03638a09a50bcb34
NikolaTesla17 <44534701+NikolaTesla17@users.noreply.github.com>
2019-09-03 08:37:03 -0700

    errors gone

--- Lab903 Repulsion/ball.js
+++ Lab903 Repulsion/ball.js
@@ -1,16 +1,15 @@
-class Ball{
-    constructor(x,y,dx,dy){
-      this.loc = createVector(x, y);
-      this.vel = createVector(dx, dy);
-      this.acc = createVector (0, 0.2);//acceleration vector add
-      this.clr = color(random(255),random(255),random(255));
-    }
-    run(){//use this so you dont have to run all of these for each ball
+function Ball(location, velocity, radius, col){
+    this.loc = location;
+    this.vel = velocity;
+    this.rad = radius;
+    this.col = col;
+    this.acc = createVector(0, 0.5);
+  this.run = function(){//use this so you dont have to run all of these for each ball
       this.checkEdges();
       this.update();
       this.render();
     }
-    checkEdges(){//check if touching edge, if so reverse direction
+    this.checkEdges = function(){//check if touching edge, if so reverse direction
       if(this.loc.x < 0){
         this.vel.x = -this.vel.x;//left
       }
@@ -24,11 +23,11 @@ class Ball{
         this.vel.y = -this.vel.y;
       }
     }
-    update(){//update location
+    this.update=function(){//update location
       this.vel.add(this.acc);
       this.loc.add(this.vel);//add velocity to make gravity
     }
-    render(){//render one frame
+    this.render = function(){//render one frame
       fill(this.clr);
       ellipse(this.loc.x, this.loc.y, 10, 10);//render ball, change if you want other shapes
     }
