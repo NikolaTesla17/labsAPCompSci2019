6781331717deee5ebc45323dafbe2bb454b4443a
NikolaTesla17 <44534701+NikolaTesla17@users.noreply.github.com>
2019-09-11 08:42:45 -0700

    commented things

--- Lab 910 Paddle Ball/ball.js
+++ Lab 910 Paddle Ball/ball.js
@@ -28,31 +28,30 @@ class Ball{
     }
     paddle(){
       var d = mouseY;
-        if((this.loc.y>=(d-50)&&(d+50)>=this.loc.y)&&this.loc.x < 31){
-        p++;
-        console.log("Score is " + p)
-        textSize(200);
-        fill(255, 255, 255);
-        text(p, 400, 400);
-        this.vel.x = -this.vel.x;
+        if((this.loc.y>=(d-50)&&(d+50)>=this.loc.y)&&this.loc.x < 31){//detect paddle hit
+        p++;//incriment score
+        textSize(200);//large score
+        fill(255, 255, 255);//make score text white
+        text(p, 400, 400);//write score
+        this.vel.x = -this.vel.x;//bounce ball
       }
     }
     update(){//update location
-      this.loc.add(this.vel);
+      this.loc.add(this.vel);//move things
     }
     win(){
       if(p>1){
-        background(5,5,255);
-        var w = "You Win!";
-        textSize(80);
-        fill(255, 255, 255);
-        text(w, 220, 400);
-        throw new Error("won");
+        background(5,5,255);//wipe background make blue
+        var w = "You Win!";//win string
+        textSize(80);//make text fit
+        fill(255, 255, 255);//make win white
+        text(w, 220, 400);//write you win
+        throw new Error("won");//stop game
       }
     }
     render(){//render one frame
-      fill(this.clr);
+      fill(this.clr);//make this color
       ellipse(this.loc.x, this.loc.y, 10, 10);//render ball, change if you want other shapes
-      background(5,5,5,25);
+      background(5,5,5,25);//give slight trail
     }
   }
--- Lab 910 Paddle Ball/sketch.js
+++ Lab 910 Paddle Ball/sketch.js
@@ -4,19 +4,15 @@
 
 var balls = [];
 function setup() {
-  var ctx = createCanvas(800, 800);
+  var ctx = createCanvas(800, 800);//make and position canvas
   ctx.position((windowWidth-width)/2, 30);
-  background(5, 5, 5,);
+  background(5, 5, 5);
   fill(200, 30, 150);
-  balls[1] = new Ball(random(width),random(height),random(-5,5),random(-5,5))
-  paddle1 = new Paddle(20,400);
+  balls[1] = new Ball(random(width),random(height),random(-5,5),random(-5,5))//make the game ball
+  paddle1 = new Paddle(20,400);//make the paddle
 }
 
 function draw() {
-ballsDraw();
-}
-
-function ballsDraw(){
-balls[1].run();
-paddle1.run();
+  balls[1].run();//runs the ball
+  paddle1.run();//runs the paddle 
 }
