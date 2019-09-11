206d1f67de08a8d4ce03706b7f5c7c23f1acabbe
NikolaTesla17 <44534701+NikolaTesla17@users.noreply.github.com>
2019-09-11 08:44:55 -0700

    all is commented
    best code ever

--- Lab 910 Paddle Ball/paddle.js
+++ Lab 910 Paddle Ball/paddle.js
@@ -1,16 +1,11 @@
 class Paddle{
     constructor(x,y){
-      this.loc = createVector(x, y);
-      //this.vel = createVector(dx, dy);
-      this.clr = color(random(255),random(255),random(255));
+      this.loc = createVector(x, y);//make paddle
+      this.clr = color(random(255),random(255),random(255));//paddle color
     }
     run(){//use this so you dont have to run all of these for each ball
-      //this.checkEdges();
-      this.render();
+      fill(this.clr);//make random color
+      rectMode(CENTER);//make it so the center moves
+      rect(this.loc.x, mouseY, 10, 100);//render paddle to mouse and over at 10
     }
-    render(){//render one frame
-      fill(this.clr);
-      rectMode(CENTER);
-      rect(this.loc.x, mouseY, 10, 100);//render ball, change if you want other shapes
-    }
-  }
+}
