5cb3238b878be23ea6a37405dd0172a554ce5194
NikolaTesla17 <44534701+NikolaTesla17@users.noreply.github.com>
2019-09-13 08:14:43 -0700

    more dunner but this time project reqirements

--- Lab 911 Paddle Ball/sketch.js
+++ Lab 911 Paddle Ball/sketch.js
@@ -1,18 +1,22 @@
 //  Nayan Smuek
 // 	8/21/19
 //  Project 9/11
-
+var z = prompt("how many balls", 2)
 var balls = [];
 function setup() {
-  var ctx = createCanvas(800, 800);//make and position canvas
-  ctx.position((windowWidth-width)/2, 30);
-  background(5, 5, 5);
+  var ctx = createCanvas(1000, 600);//make canvas
+  ctx.position((windowWidth-width)/2, 30);//put canvas in the middle
+  background(5, 5, 5);//make black background
   fill(200, 30, 150);
-  balls[1] = new Ball(random(width),random(height),random(-5,5),random(-5,5))//make the game ball
+  for(x=0;x<z;x++){
+  balls[x] = new Ball(random(width),random(height),random(-5,5),random(-5,5))//make the game ball
+  }
   paddle1 = new Paddle(20,400);//make the paddle
 }
 
 function draw() {
-  balls[1].run();//runs the ball
+  for(x=0;x<z;x++){//loop to allow for the creation of multiple balls
+  balls[x].run();//runs the ball
+  }
   paddle1.run();//runs the paddle 
 }
