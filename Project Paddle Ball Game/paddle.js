1652105a4972d7c20d5a707494ed121db2674e60
NikolaTesla17 <44534701+NikolaTesla17@users.noreply.github.com>
2019-09-16 08:33:56 -0700

    almost done with requirments

--- Project Paddle Ball Game/paddle.js
+++ Project Paddle Ball Game/paddle.js
@@ -6,6 +6,6 @@ class Paddle{
     run(){//use this so you dont have to run all of these for each ball
       fill(this.clr);//make random color
       rectMode(CENTER);//make it so the center moves
-      rect(this.loc.x, mouseY, 10, 100);//render paddle to mouse and over at 10
+      rect(mouseX, this.loc.y, 100, 10);//render paddle to mouse and over at 10
     }//end run function
 }//end the class
