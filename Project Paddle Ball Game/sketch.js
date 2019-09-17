90be45eb12b3b8e34a2209aadd73b1f52a9f1835
Nayan Smuek <nayansmuek@Nayans-MacBook-Pro.local>
2019-09-17 08:25:29 -0700

    blargh

--- Project Paddle Ball Game/sketch.js
+++ Project Paddle Ball Game/sketch.js
@@ -3,6 +3,7 @@
 //  Project 9/11
 var z = prompt("how many balls", 2)
 var balls = [];
+var l = 0;
 function setup() {
   var ctx = createCanvas(600, 600);//make canvas
   ctx.position((windowWidth-width)/2, 30);//put canvas in the middle
@@ -14,7 +15,21 @@ function setup() {
   paddle1= new Paddle(300,500);
 }
 
+function start(){
+  for(i=0;i<100;i++){
+  background(5,5,255);//wipe background make blue
+  var w = "You Win!";//win string
+  textSize(80);//make text fit
+  fill(255, 255, 255);//make win white
+  text(w, 150, 300);//write you win
+  l++;
+  }
+}
+
 function draw() {
+  if(l == 0){
+    start();
+  }
   for(x=0;x<z;x++){//loop to allow for the creation of multiple balls
   balls[x].run();//runs the ball
   }
