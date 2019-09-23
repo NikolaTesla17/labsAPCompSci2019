98887c642226f5fd6bdd8d713a4496614c4dd3e9
Nayan Smuek <nayansmuek@Nayans-MacBook-Pro.local>
2019-09-23 08:26:54 -0700

    glurp

--- Project Paddle Ball Game V2/paddle.js
+++ Project Paddle Ball Game V2/paddle.js
@@ -6,6 +6,7 @@ class Paddle{
     run(){//use this so you dont have to run all of these for each ball
       fill(this.clr);//make random color
       rectMode(CENTER);//make it so the center moves
-      rect(mouseX, this.loc.y, 100, 10);//render paddle to mouse and over at 10
+      this.loc.x = mouseX;
+      rect(this.loc.x, this.loc.y, 100, 10);//render paddle to mouse and over at 10
     }//end run function
 }//end the class
