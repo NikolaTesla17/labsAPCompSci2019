96be5b646611d1277ea41a91975f0382b6442999
Nayan Smuek <nayansmuek@Nayans-MacBook-Pro.local>
2019-09-24 08:48:54 -0700

    more requirements

--- Project Paddle Ball Game V2/paddle.js
+++ Project Paddle Ball Game V2/paddle.js
@@ -1,7 +1,7 @@
 class Paddle{
     constructor(x,y){//find where to make paddle
       this.loc = createVector(x, y);//make paddle
-      this.clr = color(random(255),random(255),random(255));//paddle color
+      this.clr = color(255,0,0);//paddle color
     }//end the constructor
     run(){//use this so you dont have to run all of these for each ball
       fill(this.clr);//make random color
