ae5226f518539c06ebece599340c361adc0d2778
Nayan Smuek <nayansmuek@Nayans-MacBook-Pro.local>
2019-09-25 08:49:56 -0700

    more done

--- Project Paddle Ball Game V2/button.js
+++ Project Paddle Ball Game V2/button.js
@@ -4,17 +4,25 @@ class button{
       this.clr = color(255,0,0);//paddle color
     }else if(id == 2){
         this.clr = color(0,255,0);//paddle color
+    }else if(id == 0){
+        this.clr = color(255,255,0);//paddle color
       }else{
         this.clr = color(0,0,255);//paddle color
       }
-
+    
+      this.id=id;
       this.loc = createVector(x, y);//make paddle
 
       this.text = text;
     }//end the constructor
     run(){//use this so you dont have to run all of these
       fill(this.clr);//make color
+      if(this.id>0){
       rect(this.loc.x, this.loc.y, 110, 50);//render paddle to mouse and over at 10
+      }
+      if(this.id==0){
+        rect(this.loc.x, this.loc.y, 150, 50);
+      }
       textSize(30);//make text fit
       fill(255,255,255);//make text color
       text(this.text, this.loc.x, this.loc.y+40);//write you win
--- Project Paddle Ball Game V2/sketch.js
+++ Project Paddle Ball Game V2/sketch.js
@@ -16,6 +16,8 @@ function setup() {
   easy = new button(100,450,"   easy",2);//55 to y 25 to x
   medium = new button(250,450,"medium",3);
   hard = new button(400,450,"   hard",1);
+  direct = new button(400,50," directions",0);
+  restart = new button(300,300," restart",1);
 }
 
 // function keyPressed() {
@@ -39,22 +41,27 @@ function start(){
   easy.run();
   medium.run();
   hard.run();
+  direct.run();
 
   textSize(70);//make text fit
   fill(255,255,255);//make text color
   text("Paddle Ball", 130, 200);//write you win
 
-    if(mouseIsPressed&&mouseX>100&&mouseX<210&&mouseY>395&&mouseY<505){
+    if(mouseIsPressed&&mouseX>100&&mouseX<210&&mouseY>450&&mouseY<505){
         mode='easy'
         console.log("easy");
       }
-    if(mouseIsPressed&&mouseX>250&&mouseX<360&&mouseY>395&&mouseY<505){
+    if(mouseIsPressed&&mouseX>250&&mouseX<360&&mouseY>450&&mouseY<505){
       mode='medium'
       console.log("medium");
     }
-    if(mouseIsPressed&&mouseX>400&&mouseX<510&&mouseY>395&&mouseY<505){
+    if(mouseIsPressed&&mouseX>400&&mouseX<510&&mouseY>450&&mouseY<505){
       mode='hard'
       console.log("hard");
+    }
+    if(mouseIsPressed&&mouseX>400&&mouseX<510&&mouseY>50&&mouseY<105){
+      mode='direct'
+      console.log("direct");
     }
 }
 
@@ -72,8 +79,36 @@ function draw() {
   }
 
   if(mode=="easy"){
+    load(5);
+    mode="ready";
+  }
+
+  if(mode=="medium"){
     load(10);
     mode="ready";
+  }
+
+  if(mode=="hard"){
+    load(20);
+    mode="ready";
+  }
+
+  if(mode=="direct"){
+    background(5,5,5);
+    textSize(20);//make text fit
+    fill(255,255,255);//make text color
+    text("Hit all of the balls", 130, 200);//write you win
+    restart.run();
+
+    if(mouseIsPressed&&mouseX>300&&mouseX<410&&mouseY>300&&mouseY<355){
+      mode='restart'
+      console.log("restart");
+    }
+  }
+
+  if(mode=="restart"){
+    //setup();
+    mode=="null";
   }
 
   if(mode=="ready"){
