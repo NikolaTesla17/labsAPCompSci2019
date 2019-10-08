f81a8210b0fbe89cd31100c0817d39505eb5d86e
Nayan Smuek <nayansmuek@Nayans-MacBook-Pro.local>
2019-10-08 14:59:06 -0700

    done

--- Lab1007CheckerBaord/StartCode/CheckerBoard/sketch.js
+++ Lab1007CheckerBaord/StartCode/CheckerBoard/sketch.js
@@ -1,25 +1,10 @@
-/*
-**Lab 911:CheckerBoard
-**
-**
-*/
-
-
-//  Global variables
-// Declare squares[] as a global variable
-
-
-// put setup code here
+//Nayan Smuek Checkerboard Lab
 var squares = [];//to make balls
 function setup() {
   var cnv = createCanvas(800, 800);
   cnv.position((windowWidth-width)/2, 30);
   background(235);
-  // Create a function that loads the squares into an
-  // array of square objects and call the function
-
   loadSquares();
-  //  traverse array and run each square
 
 var p = squares.length+1;
   for(var i = 0; i < squares.length; i++){
@@ -34,59 +19,34 @@ function loadSquares(){
     } else {
       cl = 2;
     }
-    
-
-    //add a balls.length-9* which if statment it is that plus a few other things will make code loopable
-    
     r=q;
-    if(q<=8){
-      console.log("0");
-      squares[q] = new square(r*100,0,cl);   
-    } else{
-        if(q<=16){
-        console.log("100");
-        squares[q] = new square(((r-9)*100),100,cl);   
-      }else{
-      if(q<=25){
-        console.log("200");
-        squares[q] = new square(((r-18)*100),200,cl);   
-      } else{
-        if(q<=34){
-          console.log("300");
-          squares[q] = new square(((r-27)*100),300,cl);   
-      } else{
-        if(q<=43){
-          console.log("400");
-          squares[q] = new square(((r-36)*100),400,cl);   
-      } else{
-        if(q<=52){
-          console.log("500");
-          squares[q] = new square(((r-45)*100),500,cl);   
-      } 
-      else{
-        if(q<=61){
-          console.log("600");
-          squares[q] = new square(((r-54)*100),600,cl);   
-      } else{
-        if(q<=70){
-          console.log("700");
-          squares[q] = new square(((r-63)*100),700,cl);   
-      } 
-    }
-    }
-    }
-    }
-    }
-      }
-      }
-      }
-    }
-    
 
+    switch(true){
 
+    case(r<=8): squares[q] = new square(r*100,0,cl);
+    break;
 
-  //  create a square object and push it into the squares array
+    case(q<=16):squares[q] = new square(((r-9)*100),100,cl);   
+    break;
 
-  //  create a loop that will push 64 squares into an array
-  //  calculate the x and y position interms of the loop control variable
+    case(q<=25):squares[q] = new square(((r-18)*100),200,cl); 
+    break; 
+      
+      
+    case(q<=34):squares[q] = new square(((r-27)*100),300,cl); 
+    break;
 
+    case(q<=43):squares[q] = new square(((r-36)*100),400,cl);
+    break;
+
+    case(q<=52):squares[q] = new square(((r-45)*100),500,cl);
+    break;
+
+    case(q<=61):squares[q] = new square(((r-54)*100),600,cl); 
+    break;
+
+    case(q<=70):squares[q] = new square(((r-63)*100),700,cl);
+    break;
+    }
+  }
+}
--- Lab1007CheckerBaord/StartCode/CheckerBoard/square.js
+++ Lab1007CheckerBaord/StartCode/CheckerBoard/square.js
@@ -1,5 +1,5 @@
 class square{
-  constructor(x,y,cl){//find where to make paddle
+  constructor(x,y,cl){//find where to make square and color
     this.loc = createVector(x, y);
     if(cl == 1){
     this.clr = color(255,192,203);//pink
@@ -8,8 +8,8 @@ class square{
       this.clr = color(0,128,0);//green
     }
   }//end the constructor
-  render(){//use this so you dont have to run all of these for each ball
-    fill(this.clr);//make random color
-    rect(this.loc.x, this.loc.y, 100, 100);//render paddle to mouse and over at 10
+  render(){//use this so you dont have to run all of these seperatly
+    fill(this.clr);//make board color
+    rect(this.loc.x, this.loc.y, 100, 100);//render square to location with size 100 100
   }//end run function
-}//end of paddle class
+}//end of square class
