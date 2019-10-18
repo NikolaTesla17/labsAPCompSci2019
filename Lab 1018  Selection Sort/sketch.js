7d07c2829b3f149ad6100338405f1fc600618b2b
Nayan Smuek <nayansmuek@Nayans-MacBook-Pro.local>
2019-10-18 08:36:27 -0700

    finished selection sort

--- Lab 1018  Selection Sort/sketch.js
+++ Lab 1018  Selection Sort/sketch.js
@@ -3,7 +3,7 @@ function setup() {
   var list = [];//to be sorted
 
    //code from http://stackoverflow.com/questions/962802#962890
-  for (var a=[],i=0;i<500;++i) a[i]=i;
+  for (var a=[],i=0;i<10;++i) a[i]=i;
   function shuffle(array) {
     var tmp, current, top = array.length;
     if(top) while(--top) {
@@ -16,15 +16,18 @@ function setup() {
    //end code from http://stackoverflow.com/questions/962802#962890
 iteration = 0;//to count how long it takes to sort
 
-for(i = 0;i<list.length;i++){
+for(var i = 0; i < list.length; i++){
    console.log(list);
    iteration++;
-   curent=list[i];
-   var prev;
-   for(var prev = i - 1; prev >= 0 && list[prev] > curent; prev--) {
-      list[prev+1] = list[prev];
-    }
-   list[prev+1] = curent;
+check = i;
+for(var  j = i+1; j<list.length; j++){
+ if(list[j]<list[check]){
+    check = j;
+ }
+}
+tmp = list[i];
+list[i] = list[check];
+list[check] = tmp;
 }
 console.log("done: " + iteration);//say finished and print array
 }//end setup
