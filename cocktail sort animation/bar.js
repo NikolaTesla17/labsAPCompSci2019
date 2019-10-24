713c49205421cb74ab8496b74032c609b700e618
Nayan Smuek <nayansmuek@Nayans-MacBook-Pro.local>
2019-10-24 08:44:14 -0700

    need to do things but dont wanna

--- cocktail sort animation/bar.js
+++ cocktail sort animation/bar.js
@@ -1,9 +1,12 @@
 class bar{//to make new bars easy to add more 
-    constructor(size,locX,){
+    constructor(size,locX,id){
         this.height =size;
-         this.x = locX;
+        this.x = locX;
+        this.id = id;
     }
     run(){
+        this.x = ((50*this.id));
+
         fill(255,0,0);//make red
         rectMode(CENTER);
         rect(this.x, 500, 40, this.height);
--- cocktail sort animation/sketch.js
+++ cocktail sort animation/sketch.js
@@ -1,10 +1,10 @@
 //  Nayan Smuek
 function setup() {
-   var ctx = createCanvas(800, 500);//make canvas
+   var ctx = createCanvas(550, 500);//make canvas
    ctx.position((windowWidth-width)/2, 30);//put canvas in the middle
    background(5, 5, 5);//make black background
    fill(200, 30, 150);
-   frameRate(4);
+   frameRate(2);
 
 }//end setup
 
@@ -13,70 +13,73 @@ function setup() {
 iteration = 0;//to count how long it takes to sort
 for(x=0;x<10;x++){
  var bars = [1,2,3,4,5,6,7,8,9,10];
- console.log(bars[2]);
+ var rand = [7,4,5,2,1,3,9,10,6,8];//randomly shuffle
  }
 
 
  for(x=0;x<10;x++){
     
     var move = 50*x;
-    bars[x] = new bar((100+move),(100+move))
+    //bars[x] = new bar((100+move),(100+move)) //I need to make it so th bars dont have a preordained location
+    bars[x] = new bar((100+move),(50*rand[x]),rand[x])
     }
- console.log(bars[2]);
+
+
 shuf(bars);
-console.log(bars);
-console.log(bars[2]);
 
 //  The draw function is called @ 30 fps
 function draw() {
 
+var sort = bars;
+console.log(sort);
+sortin();
+drw();
 
-   for(x=0;x<10;x++){
-      bars[x].run();
-   }
 }
 
-function bars() {
+function sortin() {
    sorting = true;//you need to sort
 while (sorting == true){//if you need to sort then
 //possible to draw stuff here
-   for (let x=0;x<list.length-1;x++){//while there is things to sort iterate up
-      if (list[x] > list[x+1]){//check if a number is to far down
-         temp = list[x];//if yes than swap
-         list[x] = list[x + 1];//swap numbers
-         list[x+1] = temp;//swap numbers
-         draw();
+   for (let x=0;x<bars.length-1;x++){//while there is things to sort iterate up
+      if (bars[x] > bars[x+1]){//check if a number is to far down
+         temp = bars[x];//if yes than swap
+         bars[x] = bars[x + 1];//swap numbers
+         bars[x+1] = temp;//swap numbers
+         console.log("drw");
+         drw();
          sorting = true;//you still need to sort 
       }//end swap
    }//stop iterating foward
 
   sorting = false;//no more sorting needed however the rest of the statment still finishes unless the itterate backwards loop sets it back to true because stuff is still out of order
 
-   for (let j=list.length-1;j > 0;j--){//iterate backwards
-         if (list[j-1] > list[j]){//if number is to far up
-         temp = list[j];//swap
-         list[j] = list[j - 1];//swap
-         list[j - 1] = temp;//swap
-         draw();
+   for (let j=bars.length-1;j > 0;j--){//iterate backwards
+         if (bars[j-1] > bars[j]){//if number is to far up
+         temp = bars[j];//swap
+         bars[j] = bars[j - 1];//swap
+         bars[j - 1] = temp;//swap
+         drw();
       }//end swap
    }//stop iterating backwards
 
-   for (let j=list.length-1;j > 0;j--){//iterate backwards
-      if (list[j-1] > list[j]){//if number is to far up
+   for (let j=bars.length-1;j > 0;j--){//iterate backwards
+      if (bars[j-1] > bars[j]){//if number is to far up
          sorting = true;//sorting still needed
       }//this is needed so the sort dosn't end up doing a whole extra iteration sometimes
    }//stop iterating
    iteration++;//say you did one loop
-   console.log("iteration: " + iteration + "\n arrary: " + list);//print out list at iteration to show change
-
 }//if sorting isnt true
-console.log("done: " + list);//say finished and print array
 }
 
 
 
 
-
+function drw(){
+   for(x=0;x<10;x++){
+      bars[x].run();
+   }
+}
 
 
 function shuf(a) {
@@ -87,6 +90,5 @@ function shuf(a) {
        a[i] = a[j];
        a[j] = x;
    }
-   console.log(a[2]);
    return a;
 }
