326a95e33d293e11684a2124ce3b34dd44f9adb8
Nayan Smuek <nayansmuek@Nayans-MacBook-Pro.local>
2019-11-04 08:50:04 -0800

    mostly workign, excpet for middle segments

--- Project SnakeGame/snake.js
+++ Project SnakeGame/snake.js
@@ -5,7 +5,12 @@ class snake{
         this.segs = 1;
         this.lastx;
         this.lasty;
+        this.last = [1,0];
+
+        //let snake = [  {x: 150, y: 150},  {x: 140, y: 150},  {x: 130, y: 150},  {x: 120, y: 150},  {x: 110, y: 150},];
+        this.y = 0;
     }
+
 run(){
     this.change();
     this.render();
@@ -16,26 +21,27 @@ change(){
         if (keyCode === LEFT_ARROW) {
             //replace this tomfoolery with an array when you have time
             //declare a new vector every time with the value of the thing in front of it
-             var y = this.seg.length;
-             this.last = createVector(this.loc.x,this.loc.y);
+             this.y++;
+             console.log(this.y);
+             this.last[this.y] = createVector(this.loc.x,this.loc.y);
              this.loc.x=this.loc.x-20;
             console.log("left");
         }
         if (keyCode === RIGHT_ARROW) {
-            var y = this.seg.length;
-            this.last = createVector(this.loc.x,this.loc.y);
+            this.y++;
+            this.last[this.y] = createVector(this.loc.x,this.loc.y);
             this.loc.x=this.loc.x+20;
           console.log("right");
         }
         if (keyCode === DOWN_ARROW) {
-            var y = this.seg.length;
-            this.last = createVector(this.loc.x,this.loc.y);
+            this.y++;
+            this.last[this.y] = createVector(this.loc.x,this.loc.y);
             this.loc.y=this.loc.y+20;
           console.log("down");
         }
         if (keyCode === UP_ARROW) {
-            var y = this.seg.length;
-            this.last = createVector(this.loc.x,this.loc.y);
+            this.y++;
+            this.last[this.y] = createVector(this.loc.x,this.loc.y);
             this.loc.y=this.loc.y-20;
           console.log("up");
         }
@@ -50,10 +56,31 @@ segment(){
     }
     // if(this.seg>this.segs){  
     // }
-    for(var x=1;x<this.seg;x++){
+    console.log(this.last.length);
+    for(var x=1;x<this.last.length;x++){
+        var z = this.last.length;
         fill(255,100,0);//make random color
-        rect(this.last.x,this.last.y, 20, 20);
+        if(this.seg>1){
+        rect(this.last[z-(this.seg-1)].x,this.last[z-(this.seg-1)].y, 20, 20);
+        }
     }
+
+
+
+
+    // for(var x=1;x<this.last.length;x++){
+    //     var x = this.last.length;
+    //     fill(255,100,0);//make random color
+    //     rect(this.last[x-2].x,this.last[x-2].y, 20, 20);
+    // }
+
+
+
+
+
+
+
+
 }
 render(){
     fill(255,255,0);//make random color
