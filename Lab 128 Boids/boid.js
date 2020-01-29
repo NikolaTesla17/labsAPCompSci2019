df88482ec49a58d830cc194578dc8a33b952cddb
Nayan Smuek <nayansmuek@nayan.local>
2020-01-29 08:36:04 -0800

    actually works now
    I forgot the blood sacrafices to the dark all lord

--- Lab 128 Boids/boid.js
+++ Lab 128 Boids/boid.js
@@ -1,9 +1,10 @@
 class boid{
-    constructor(x,y,dx,dy){
+    constructor(x,y,dx,dy,q){
       this.loc = createVector(x, y);
       this.vel = createVector(dx, dy);
-      this.acc = createVector (0,0.2);
+      this.acc = createVector (random(-1,1),0.2);
       this.clr = color(random(255),random(255),random(255));
+      this.id = q;
     }
     run(){//use this so you dont have to run all of these for each ball
       this.checkEdges();
@@ -13,15 +14,19 @@ class boid{
     checkEdges(){//check if touching edge, if so reverse direction
       if(this.loc.x < 0){
         this.vel.x = -this.vel.x;
+        this.acc.x = -this.acc.x;
       }
       if(this.loc.x > width){
         this.vel.x = -this.vel.x;
+        this.acc.x = -this.acc.x;
       }
       if(this.loc.y < 0){
         this.vel.y = -this.vel.y;
+        this.acc.y = -this.acc.y;
       }
       if(this.loc.y > height){
         this.vel.y = -this.vel.y;
+        this.acc.y = -this.acc.y;
       }
     }
     update(){//update location
@@ -30,19 +35,22 @@ class boid{
       this.vel.limit(5);
     }
     render(){//render one frame
-      fill(this.clr);
+      fill('red');
 
+      //for(var i=0;i<10;i++){
       push();
       translate(this.loc.x, this.loc.y);
       noStroke();
-      for (let i = 0; i < 10; i ++) {
-        ellipse(0, 30, 20, 80);
+      for (let z = 0; z < 40; z ++) {
+        ellipse(0, 6, 4, 16);
         rotate(PI/5);
       }
       pop();
-      if((this.loc.x < boid[0].loc.x + 100&&this.loc.x > boid[0].loc.x - 100)&&(this.loc.y < boid[0].loc.y + 100&&this.loc.y > boid[0].loc.y - 100)){
 
+      for(let i=0;i<40;i++)
+      if((this.loc.x < boids[i].loc.x + 100&&this.loc.x > boids[i].loc.x - 100)&&(this.loc.y < boids[i].loc.y + 100&&this.loc.y > boids[i].loc.y - 100)){
+        fill('red');
+        line(this.loc.x, this.loc.y, boids[i].loc.x, boids[i].loc.y);
       }
       }
     }
-  }
--- Lab 128 Boids/sketch.js
+++ Lab 128 Boids/sketch.js
@@ -3,6 +3,7 @@
 //  Project 8/22
 
 var boids = [];
+var iter = 0;
 function setup() {
   var cnv = createCanvas(800, 800);
   cnv.position((windowWidth-width)/2, 30);
@@ -13,19 +14,22 @@ function setup() {
 
 
 function draw() {
-  //for(i=1;i<3;i++){} put other function inside this to run at 60fps
-ballsDraw();
+  background(255,255,255,50)//get rid of ball trails
+  for(;iter<40;iter++){
+    boids[iter].run();
+  }
+  iter = 0;
 }
 
-function ballsDraw(){
-  background(5,5,5,50)//get rid of ball trails
-  for(i=0;i<1;i++){
-    boids[i].run();
-  }
-}
+// function ballsDraw(){
+//   background(5,5,5,50)//get rid of ball trails
+//   for(i=0;i<10;i++){
+//     boids[i].run();
+//   }
+// }
 
 function loadBoids(){
-  for(q=0;q<1;q++){ //create array with number of balls wanted
-    boids[q] = new boid(random(width),random(height),random(-5,5),random(-5,5))
+  for(q=0;q<40;q++){ //create array with number of balls wanted
+    boids[q] = new boid(random(width),random(height),random(-5,5),random(-5,5), q)
   }
 }
